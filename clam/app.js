09a5e8030ca5146cfe437ebd100e7abcbc4e3c75
David Zheng <dzd123@mit.edu>
2015-10-13 20:27:34 -0400

    Work in Progress

--- clam/app.js
+++ clam/app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var io = require('socket.io')('http');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -56,6 +57,10 @@ app.use(function(err, req, res, next) {
     message: err.message,
     error: {}
   });
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
 http.createServer(app).listen(app.get('port'), function(){
--- clam/routes/index.js
+++ clam/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var counter = 0;
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Clam' });
 });
