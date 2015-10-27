4eb3e7a23feeb56192590a7f202e1839ad74f0ce
stevenhao <stevenhao@gmail.com>
2015-10-27 17:16:39 -0400

    update tester to use new register protocol

--- clam/public/javascripts/tester.js
+++ clam/public/javascripts/tester.js
@@ -1,5 +1,6 @@
+myGid = 0;
 window.onload = function() {
-  var socket = io();
+  socket = io();
   console.log('tester.js is alive.');
 
   sendMessage = function(ev, msg) {
@@ -14,7 +15,7 @@ window.onload = function() {
   }
 
   $('.view').html('Hi');
-  $('form').submit(function(){
+  $('#sendform').submit(function(){
     ev = $('#event').val();
     value = $('#value').val();
     // console.log(value);
@@ -31,7 +32,10 @@ window.onload = function() {
 
   console.log('done');
 
-  var listenTo = ['connection', 'register error', 'register success', 'chat response', 'pid error', 'pid success', 'guess error', 'guess success', 'flip error', 'flip success', 'pass error', 'pass success'];
+  var listenTo = ['connection', 'register error',
+   'register success', 'chat response', 'pid error', 'pid success', 
+   'guess error', 'guess success', 'flip error', 'flip success', 
+   'pass error', 'pass success', 'create success', 'gameIDs'];
   makeHandler = function(event) {
     return function(data) {
       receiveMessage(event, data);
@@ -41,5 +45,61 @@ window.onload = function() {
     console.log('listening to events: '+ e);
     socket.on(e, makeHandler(e));
   }
+
+  print = console.log.bind(console);
+
+  $('#register').submit(function() {
+    myPid = parseInt($('input[name=pid]:checked').val());
+    myGid = $('input[name=gid]:checked').val();
+    if (myGid == 'New') {
+      print('creating game');
+      socket.on('create success', function(gid) {
+        myGid = gid;
+        updateGameList();
+        updateGame();
+      });
+      sendMessage('create', {'num_players': 4, 'num_ranks': 12, 'num_colors': 2});
+    } else {
+      print('playing as pid, gid:', myPid, myGid);
+      updateGame();
+    }
+    return false;
+  });
+
+  socket.on('gameIDs', function(list) {
+    renderGameList(list);
+  });
+  updateGameList();
 };
 
+function updateGame() {  
+  print('updating game');
+  socket.emit('register', myPid, myGid);
+  $('#messages').append($('<li>').text('sent[register]: ' + myPid + ',' + myGid));
+  console.log('sent message register', myPid, myGid);  
+
+}
+
+
+function updateGameList() {
+  print('updating game list');
+  sendMessage('gameIDs',{});
+
+}
+
+renderGameList = function(list) {
+  print('rendering game list', list);
+  var gidform = $('#gidform');
+  gidform.html('');
+  list.push('New');
+  for (var i = 0; i < list.length; ++i) {
+    gid = list[i];
+    var el = $('<input type="radio" name="gid" />');
+    el.attr('value', gid);
+    if (gid == myGid) {
+      el.attr('checked', 'checked');
+    }
+    gidform.append(el);
+    gidform.append(gid);
+  }
+}
